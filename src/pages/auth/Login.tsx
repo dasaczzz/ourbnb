7c5162ac116337425fe634a9507b9c174f4d4565
dasczzz <danielscz.dsa@gmail.com>
2025-04-26 12:49:19 -0500

    fix: setting good the login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,26 +1,34 @@
+import { useState } from 'react'
+import { LoginForm } from '../../components/auth/LoginForm'
+import { RegisterModal } from '../../components/auth/RegisterModal'
+
 export const Login = () => {
+
+  const [open, setOpen] = useState(false)
+
+  // controls for modal
+  const handleOpenModal = () => { setOpen(true) }
+  const handleCloseModal = () => { setOpen(false) }
+
   return (
-    <div className="flex flex-col w-1/2 bg-secondary-200 h-screen justify-center items-center gap-2.5">
-      <section className="w-1/2">
-        <div className="flex gap-2.5 justify-center items-center">
-          <img src="ourbnb.svg" alt="hello" />
-          <h1 className="text-4xl font-bold text-primary-400">Ourbnb</h1>
+    <div className="flex flex-col w-1/2 bg-secondary-200 h-screen justify-center items-center">
+      <section className="w-1/2 flex flex-col">
+        {/* logo and title  */}
+        <div className="flex gap-4 justify-center items-center">
+          <img src="ourbnb.svg" className='w-32 h-24' alt="logo empresarial" />
+          <h1 className="font-bold text-primary-400 text-6xl">Ourbnb</h1>
         </div>
-        <form className="flex flex-col p-6">
-          <label className="text-lg mb-1">Correo electronico</label>
-          <input type="email" name="email" className="shadow-sm rounded-md px-4 py-2 text-secondary-400 mb-4 focus:outline-1 outline-secondary-400" placeholder="ejemplo@mail.com"/>
 
-          <label className="text-lg mb-1">Contraseña</label>
-          <input type="email" name="password" className="shadow-sm rounded-md px-4 py-2 text-secondary-400 mb-6 focus:outline-1 outline-secondary-400" placeholder="Tu contraseña"/>
+        <LoginForm />
 
-          <button className="w-full bg-primary-300 rounded-md text-secondary-200 text-lg font-medium py-3 hover:bg-primary-400">Entrar</button>
-        </form>
+        {/* Create account */}
         <hr className="mx-6"/>
         <div className="mt-4 flex justify-center gap-2 text-lg">
           <span>¿No tienes una cuenta?</span>
-          <button className="hover:underline underline-offset-4 hover:text-primary-300" >crear una cuenta</button>
+          <button onClick={handleOpenModal} className="hover:underline underline-offset-4 hover:text-primary-300" >crear una cuenta</button>
         </div>
       </section>
+      <RegisterModal isOpen={open} closeModal={handleCloseModal}/>
     </div>
   )
 }
