13ccf404c4f46b4cd97ed3d8733f113db2062f4e
dasczzz <danielscz.dsa@gmail.com>
2025-04-28 21:30:28 -0500

    feat: adding redux stuff, before backend

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -1,16 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+enum states {
+  // eslint-disable-next-line no-unused-vars
+  AUTHENTICATED, NOT_AUTHENTICATED, CHECKING
+}
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    value: 0,
+    status: states.NOT_AUTHENTICATED,
+    email: null,
+    name: null,
+    phone: null,
+    password: null,
+    confirmPassowrd: null,
+    token: 0,
+    errorMessage: null
   },
+
   reducers: {
-    increment: (state) => {
-      state.value += 1
+    register: (state, {payload}) => {
+      state.status = states.AUTHENTICATED
+      state.email = payload.email
+      state.name = payload.name
+      state.phone = payload.phone
+      state.password = payload.password
+      state.confirmPassowrd = payload.confirmPassword
+      state.errorMessage = null
+      state.token = Math.floor(Math.random() * 100)
     },
+
+    checkingCredentials: (state) => {
+      state.status = states.CHECKING
+    }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment } = authSlice.actions
+export const { register } = authSlice.actions
