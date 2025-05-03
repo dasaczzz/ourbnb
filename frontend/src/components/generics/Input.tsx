c511bf3a6ba0a60d960c0fac1e6f083ed33a8ea4
dasczzz <danielscz.dsa@gmail.com>
2025-05-03 18:32:43 -0500

    fix: change bg color to white

--- frontend/src/components/generics/Input.tsx
+++ frontend/src/components/generics/Input.tsx
@@ -10,7 +10,7 @@ export const Input: React.FC<props> = ({text, error, ...props}) => {
     <label className="flex flex-col w-full items-start gap-2">
       {text}
       <div className='flex flex-col w-full gap-1'>
-        <input className={`${error ? 'outline-1 outline-red-400': ''}  shadow-sm bg-secondary-200 w-full rounded-md px-4 py-2 text-secondary-400  focus:outline-1 focus:outline-secondary-400`} {...props}/>
+        <input className={`${error ? 'outline-1 outline-red-400': ''}  shadow-sm bg-white w-full rounded-md px-4 py-2 text-secondary-400  focus:outline-1 focus:outline-secondary-400`} {...props}/>
         {error && <span className='text-sm text-red-400'>{error}</span >}
       </div>
     </label>
