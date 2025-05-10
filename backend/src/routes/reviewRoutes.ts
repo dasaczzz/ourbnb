7c1ea8cf047e080aad333566caf788519b01942b
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-09 21:29:34 -0500

    feat: adding review get getReviewsByUserId route

--- backend/src/routes/reviewRoutes.ts
+++ backend/src/routes/reviewRoutes.ts
@@ -5,6 +5,7 @@ const router = Router()
 
 router.post('/reviews', reviewController.createReview)
 router.get('/reviewsByPost/:post_id', reviewController.getReviewsByPostId)
+router.get('/reviewsByUser/:user_id', reviewController.getReviewsByUserId)
 router.delete('/reviews/:id', reviewController.deleteReviewById)
 router.put('/reviews/:id', reviewController.updateReviewById)
 
