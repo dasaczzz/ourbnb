4146aec775f06cef7d59db2c7c0b32b3076edc4b
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-09 20:50:18 -0500

    fix: change route error

--- backend/src/routes/reviewRoutes.ts
+++ backend/src/routes/reviewRoutes.ts
@@ -5,6 +5,6 @@ const router = Router()
 
 router.post('/reviews', reviewController.createReview)
 router.get('/reviewsByPost/:post_id', reviewController.getReviewsByPostId)
-router.delete('/reviews:id', reviewController.deleteReviewById)
+router.delete('/reviews/:id', reviewController.deleteReviewById)
 
 export default router
