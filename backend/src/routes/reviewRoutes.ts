ff91a0aa2c40637030ae2e8a9570e80ad30e1634
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-09 20:43:40 -0500

    feat: adding delete route for reviews

--- backend/src/routes/reviewRoutes.ts
+++ backend/src/routes/reviewRoutes.ts
@@ -5,5 +5,6 @@ const router = Router()
 
 router.post('/reviews', reviewController.createReview)
 router.get('/reviewsByPost/:post_id', reviewController.getReviewsByPostId)
+router.delete('/reviews:id', reviewController.deleteReviewById)
 
 export default router
