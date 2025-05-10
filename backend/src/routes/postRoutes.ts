22de0ab868ffcce3e0dbb6b9bf9e0189accdae80
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-10 12:19:13 -0500

    fix: change route name not working

--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -6,7 +6,7 @@ const router = Router()
 router.post('/posts', postController.createPost)
 router.get('/posts', postController.getAllPosts)
 router.get('/posts/:id', postController.getPostById)
-router.get('/posts/:user_id', postController.getPostsByUserId)
+router.get('/postsByUser/:user_id', postController.getPostsByUserId)
 router.delete('/posts/:id', postController.deletePostById)
 router.put('/posts/:id', postController.updatePostById)
 
