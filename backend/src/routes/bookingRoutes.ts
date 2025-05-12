8ceb17c5de38d487fc92adc20fe29e7087b067ed
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-11 20:57:05 -0500

    feat: adding getBookingsByUserId route

--- backend/src/routes/bookingRoutes.ts
+++ backend/src/routes/bookingRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 router.post("/bookings", bookingController.createBooking);
 router.get("/bookings", bookingController.getAllBookings);
 router.get("/bookings/:id", bookingController.getBookingById);
+router.get("/bookingsByUser/:user_id", bookingController.getBookingsByUserId);
 router.delete("/bookings/:id", bookingController.deleteBookingById);
 
 export default router;
