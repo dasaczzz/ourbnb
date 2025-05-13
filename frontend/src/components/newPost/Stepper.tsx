d264ae1bff197eeeb798dc93be5ad886c7fbcb39
dasaczzz <118865516+dasaczzz@users.noreply.github.com>
2025-05-13 18:28:20 -0500

    fix: update stepper

--- frontend/src/components/newPost/Stepper.tsx
+++ frontend/src/components/newPost/Stepper.tsx
@@ -1,36 +1,31 @@
 export const Stepper = () => {
   return (
+    <ol className="container items-center flex justify-around w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0 rtl:space-x-reverse">
+      <li className="flex flex-col gap-2 items-center text-primary-400 space-x-2.5 rtl:space-x-reverse">
+        <span className="flex items-center justify-center w-10 h-10 border-2 border-primary-400 text-primary-400 text-lg font-semibold rounded-full shrink-0">
+          1
+        </span>
+        <span>
+          <h3 className="font-bold text-xl leading-tight">Describe tu espacio</h3>
+        </span>
+      </li>
+      <li className="flex flex-col gap-2 items-center text-secondary-400 space-x-2.5 rtl:space-x-reverse">
+        <span className="flex items-center justify-center w-10 h-10 border-2 border-secondary-400 text-secondary-400 text-lg font-medium rounded-full shrink-0">
+          2
+        </span>
+        <span>
+          <h3 className="font-medium text-lg leading-tight">Haz que destaque</h3>
+        </span>
+      </li>
+      <li className="flex flex-col gap-2 items-center text-secondary-400 space-x-2.5 rtl:space-x-reverse">
+        <span className="flex items-center justify-center w-10 h-10 border-2 border-secondary-400 text-secondary-400 text-lg font-medium rounded-full shrink-0">
+          3
+        </span>
+        <span>
+          <h3 className="font-medium text-lg leading-tight">Terminar y publicar</h3>
+        </span>
+      </li>
 
-    <ol className="container items-center flex justify-between w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0 rtl:space-x-reverse">
-      <li className="flex items-center text-blue-600 dark:text-blue-500 space-x-2.5 rtl:space-x-reverse">
-      <span className="flex items-center justify-center w-8 h-8 border border-blue-600 rounded-full shrink-0 dark:border-blue-500">
-        1
-      </span>
-      <span>
-        <h3 className="font-medium leading-tight">User info</h3>
-        <p className="text-sm">Step details here</p>
-      </span>
-    </li>
-        <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5 rtl:space-x-reverse">
-            <span className="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
-                2
-            </span>
-            <span>
-                <h3 className="font-medium leading-tight">Company info</h3>
-                <p className="text-sm">Step details here</p>
-            </span>
-        </li>
-        <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5 rtl:space-x-reverse">
-            <span className="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
-                3
-            </span>
-            <span>
-                <h3 className="font-medium leading-tight">Payment info</h3>
-                <p className="text-sm">Step details here</p>
-            </span>
-        </li>
     </ol>
-
-
   )
 }
