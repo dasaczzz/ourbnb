0976d2007c1181101d26b5b37f0598f045d839cd
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-14 12:04:59 -0500

    feat: adding Link for postDetail

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -4,6 +4,7 @@ import { AppDispatch, RootState } from '../store/store'
 import { startGetPosts } from '../store/thunks/postThunk'
 import { PostState } from '../store/slices/postSlice'
 import { HostCard } from '../components/index/HostCard'
+import { Link } from 'react-router-dom'
 
 export const Index = () => {
 
@@ -21,7 +22,9 @@ export const Index = () => {
   return (
     <section className="container flex justify-between items-start content-start flex-wrap py-20">
       {posts.map((item: PostState) => (
+        <Link to={`/post/${item.id}`} key={item.id}>
         <HostCard post={item}/>
+        </Link>
       ))}
     </section>
   )
