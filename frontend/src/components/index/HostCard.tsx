ccf000f76a7e54591e291a3751561f9fd81eb777
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-16 20:00:06 -0500

    fix: styles from HostCard

--- frontend/src/components/index/HostCard.tsx
+++ frontend/src/components/index/HostCard.tsx
@@ -6,14 +6,14 @@ interface props {
 
 export const HostCard: React.FC<props> = ({post}) => {
 
-  const priceToPrint = post.night_cost.toLocaleString('es-MX', {
+  const priceToPrint = (post.night_cost ?? 0).toLocaleString('es-MX', {
   style: 'currency',
   currency: 'COP',
   })
 
   return (
-    <article className="flex flex-col w-3xs rounded-full">
-      <img src={post.images[0]} alt="Hospedaje" className='object-cover object-center size-64 aspect-square'/>
+    <article className="flex flex-col w-3xs rounded-2xl shadow-md">
+      <img src={post.images?.[0] ?? ''} alt="Hospedaje" className="w-full h-50 object-cover rounded-xl"/>
       <div className='flex flex-col p-2 items-start gap-3 bg-secondary-200 '>
         <span className='text-xl font-bold text-secondary-500'>{post.title}</span>
         <span className='text-lg text-secondary-500'><b className='font-bold'>{priceToPrint}</b> noche</span>
