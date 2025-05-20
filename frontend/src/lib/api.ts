814e5200bd01f2e4d4753918b0e0e523f5c7ced0
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-19 20:28:11 -0500

    feat: adding fetchBookingsUsersValidate

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -241,3 +241,22 @@ export const fetchPostsByUser = async (user_id: string) => {
   const data = await response.json()
   return data
 }
+
+export const fetchBookingsUsersValidate = async (huespedes: string[]) => {
+  const response = await fetch(`http://localhost:4000/bookingsUsersValidate`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include',
+    body: JSON.stringify(huespedes)
+  })
+
+  const data = await response.json()
+
+  if (!response.ok) {
+    throw data
+  }
+
+  return data
+}
