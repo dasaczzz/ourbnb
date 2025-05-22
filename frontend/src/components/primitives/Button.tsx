e9265e4c32c20f7a523944eb56168d3ca23757bf
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-21 19:17:01 -0500

    style: update Button component styles for improved aesthetics and usability

--- frontend/src/components/primitives/Button.tsx
+++ frontend/src/components/primitives/Button.tsx
@@ -1,14 +1,14 @@
 import { cva, type VariantProps } from 'cva'
 
 const variants = cva(
-  'w-full rounded-md text-lg font-medium py-3 cursor-pointer transition-colors',
+  'w-full rounded-xl text-lg font-medium py-3 cursor-pointer transition-colors',
   {
     variants: {
       intent: {
         primary: 'bg-primary-400  text-secondary-200  hover:bg-primary-300',
-        secondary: ' outline-1 text-primary-400 outline-primary-400 hover:text-primary-500 hover:outline-primary-500 hover:underline',
+        secondary: 'shadow-xl w-full bg-gradient-to-r from-[#2c6d67] to-blue-500 text-white font-semibold py-3 rounded-3xl hover:opacity-80 transition',
         strong: 'bg-primary-500  text-secondary-200  hover:bg-primary-400',
-        cancel: 'outline-1 text-primary-400 outline-primary-400 hover:text-red-400 hover:outline-red-400'
+        cancel: 'bg-gradient-to-r from-[#800000] to-[#d15700] text-white text-sm py-2 rounded-lg hover:opacity-80 transition'
       }
     }
   }
