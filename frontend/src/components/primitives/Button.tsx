591b06c75b694d0e3af1f05d73049a953299fe63
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-22 18:24:12 -0500

    fix: button styles

--- frontend/src/components/primitives/Button.tsx
+++ frontend/src/components/primitives/Button.tsx
@@ -6,9 +6,11 @@ const variants = cva(
     variants: {
       intent: {
         primary: 'bg-primary-400  text-secondary-200  hover:bg-primary-300',
-        secondary: 'shadow-xl w-full bg-gradient-to-r from-[#2c6d67] to-blue-500 text-white font-semibold py-3 rounded-3xl hover:opacity-80 transition',
+        secondary: ' outline-1 text-primary-400 outline-primary-400 hover:text-primary-500 hover:outline-primary-500 hover:underline',
+        fade: 'shadow-xl w-full bg-gradient-to-r from-[#2c6d67] to-blue-500 text-white font-semibold py-3 rounded-3xl hover:opacity-80 transition',
         strong: 'bg-primary-500  text-secondary-200  hover:bg-primary-400',
-        cancel: 'bg-gradient-to-r from-[#800000] to-[#d15700] text-white text-sm py-2 rounded-lg hover:opacity-80 transition'
+        cancel: 'outline-1 text-primary-400 outline-primary-400 hover:text-red-400 hover:outline-red-400',
+        cancelFade: 'bg-gradient-to-r from-[#800000] to-[#d15700] text-white text-sm py-2 rounded-lg hover:opacity-80 transition'
       }
     }
   }
