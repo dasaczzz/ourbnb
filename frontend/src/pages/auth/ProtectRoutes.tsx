2fab977560c300a2e843ba467b8ed89f2a317e03
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-05-30 08:32:31 -0500

    fix: state error fixed for deploy

--- frontend/src/pages/auth/ProtectRoutes.tsx
+++ frontend/src/pages/auth/ProtectRoutes.tsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux'
 import { Navigate, Outlet } from 'react-router-dom'
 
 export const ProtectedRoutes = () => {
-  const { status } = useSelector(state => state.auth)
+  const { status } = useSelector((state:any ) => state.auth)
 
   if (status === 'checking') {
     return <div className="flex items-center justify-center h-screen">Cargando...</div>
